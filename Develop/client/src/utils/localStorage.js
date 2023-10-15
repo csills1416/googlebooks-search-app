bd6e3194088f5c647aa07680b6c3e29d7b0f47d9
CSillus <71670415+csills1416@users.noreply.github.com>
2023-10-15 09:10:43 -0400

    improved error handling, reduced repeates, wrapped local storage

--- Develop/client/src/utils/localStorage.js
+++ Develop/client/src/utils/localStorage.js
@@ -1,30 +1,42 @@
+// Retrieve saved book IDs from localStorage
 export const getSavedBookIds = () => {
-  const savedBookIds = localStorage.getItem('saved_books')
-    ? JSON.parse(localStorage.getItem('saved_books'))
-    : [];
-
-  return savedBookIds;
-};
-
-export const saveBookIds = (bookIdArr) => {
-  if (bookIdArr.length) {
-    localStorage.setItem('saved_books', JSON.stringify(bookIdArr));
-  } else {
-    localStorage.removeItem('saved_books');
+  try {
+    const savedBookIds = localStorage.getItem('saved_books');
+    return savedBookIds ? JSON.parse(savedBookIds) : [];
+  } catch (err) {
+    console.error('Error getting saved book IDs:', err);
+    return [];
   }
 };
 
-export const removeBookId = (bookId) => {
-  const savedBookIds = localStorage.getItem('saved_books')
-    ? JSON.parse(localStorage.getItem('saved_books'))
-    : null;
+// Save book IDs array to localStorage or remove if the array is empty
+export const saveBookIds = (bookIdArr) => {
+  try {
+    if (bookIdArr.length) {
+      localStorage.setItem('saved_books', JSON.stringify(bookIdArr));
+    } else {
+      localStorage.removeItem('saved_books');
+    }
+  } catch (err) {
+    console.error('Error saving book IDs:', err);
+  }
+};
 
-  if (!savedBookIds) {
+// Remove a specific book ID from saved book IDs in localStorage
+export const removeBookId = (bookId) => {
+  try {
+    const savedBookIds = getSavedBookIds();
+
+    if (!savedBookIds.length) {
+      return false;
+    }
+
+    const updatedSavedBookIds = savedBookIds.filter(savedBookId => savedBookId !== bookId);
+    localStorage.setItem('saved_books', JSON.stringify(updatedSavedBookIds));
+
+    return true;
+  } catch (err) {
+    console.error('Error removing book ID:', err);
     return false;
   }
-
-  const updatedSavedBookIds = savedBookIds?.filter((savedBookId) => savedBookId !== bookId);
-  localStorage.setItem('saved_books', JSON.stringify(updatedSavedBookIds));
-
-  return true;
 };
